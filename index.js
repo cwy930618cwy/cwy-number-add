ae09eb7ebab13a106b153783f14fc55a538adbbc
cwy <834415728@qq.com>
2021-12-24 10:51:16 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,27 @@
+/*!
+ * cwy-number-add <https://github.com/cwy/cwy-number-add>
+ *
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+/**
+ * add number
+ *
+ * ```js
+ * addNumber(1,2){
+ *  return 1+2
+ * }
+ * //=> 3
+ *
+ *
+ * @param {Number} `target`
+ * @param {Number} `number`
+ * @return {Number}
+ */
+
+module.exports = function addNumber(target, number) {
+  return target + number
+};
